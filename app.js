e0b181b2a3a5f8eccfc27845deae2e91b6c79380
mtorrealbagarcia@gmail.com <mtorrealbagarcia@gmail.com>
2024-06-11 21:03:24 -0400

    done

--- app.js
+++ app.js
@@ -3,68 +3,70 @@ const yargs = require('yargs')
 const notes = require('./notes.js')
 
 // Customize yargs version
+// This sets the version of the yargs package, which is used for command-line argument parsing
 yargs.version('1.1.0')
 
 // Create add command
 yargs.command({
-    command: 'add',
-    describe: 'Add a new note',
+    command: 'add', // Name of the command
+    describe: 'Add a new note', // Description of the command
     builder: {
         title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
+            describe: 'Note title', // Description for the title argument
+            demandOption: true, // Makes the title argument required
+            type: 'string' // Specifies that the title argument should be a string
         },
         body: {
-            describe: 'Note body',
-            demandOption: true,
-            type: 'string'
+            describe: 'Note body', // Description for the body argument
+            demandOption: true, // Makes the body argument required
+            type: 'string' // Specifies that the body argument should be a string
         }
     },
     handler(argv) {
-        notes.addNote(argv.title, argv.body)
+        notes.addNote(argv.title, argv.body) // Calls the addNote function from notes.js with the title and body arguments
     }
 })
 
 // Create remove command
 yargs.command({
-    command: 'remove',
-    describe: 'Remove a note',
+    command: 'remove', // Name of the command
+    describe: 'Remove a note', // Description of the command
     builder: {
         title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
+            describe: 'Note title', // Description for the title argument
+            demandOption: true, // Makes the title argument required
+            type: 'string' // Specifies that the title argument should be a string
         }
     },
     handler(argv) {
-        notes.removeNote(argv.title)
+        notes.removeNote(argv.title) // Calls the removeNote function from notes.js with the title argument
     }
 })
 
 // Create list command
 yargs.command({
-    command: 'list',
-    describe: 'List your notes',
+    command: 'list', // Name of the command
+    describe: 'List your notes', // Description of the command
     handler() {
-        notes.listNotes()
+        notes.listNotes() // Calls the listNotes function from notes.js with no arguments
     }
 })
 
 // Create read command
 yargs.command({
-    command: 'read',
-    describe: 'Read a note',
+    command: 'read', // Name of the command
+    describe: 'Read a note', // Description of the command
     builder: {
         title: {
-            describe: 'Note title',
-            demandOption: true,
-            type: 'string'
+            describe: 'Note title', // Description for the title argument
+            demandOption: true, // Makes the title argument required
+            type: 'string' // Specifies that the title argument should be a string
         }
     },
     handler(argv) {
-        notes.readNote(argv.title)
+        notes.readNote(argv.title) // Calls the readNote function from notes.js with the title argument
     }
 })
 
+// Parse the arguments and execute the corresponding command
 yargs.parse()
--- notes.js
+++ notes.js
@@ -1,71 +1,81 @@
-const fs = require('fs')
-const chalk = require('chalk')
+const fs = require('fs') // File system module for reading and writing files
+const chalk = require('chalk') // Chalk module for colored console output
 
+// Function to add a new note
 const addNote = (title, body) => {
-    const notes = loadNotes()
-    const duplicateNote = notes.find((note) => note.title === title)
+    const notes = loadNotes() // Load existing notes
+    const duplicateNote = notes.find((note) => note.title === title) // Check for duplicate titles
 
     if (!duplicateNote) {
+        // If no duplicate, add new note
         notes.push({
             title: title,
             body: body
         })
-        saveNotes(notes)
-        console.log(chalk.green.inverse('New note added!'))
+        saveNotes(notes) // Save updated notes array
+        console.log(chalk.green.inverse('New note added!')) // Success message
     } else {
-        console.log(chalk.red.inverse('Note title taken!'))
+        console.log(chalk.red.inverse('Note title taken!')) // Error message if duplicate title
     }
 }
 
+// Function to remove a note
 const removeNote = (title) => {
-    const notes = loadNotes()
-    const notesToKeep = notes.filter((note) => note.title !== title)
+    const notes = loadNotes() // Load existing notes
+    const notesToKeep = notes.filter((note) => note.title !== title) // Filter out the note to be removed
 
     if (notes.length > notesToKeep.length) {
-        console.log(chalk.green.inverse('Note removed!'))
-        saveNotes(notesToKeep)
+        // If a note was removed
+        console.log(chalk.green.inverse('Note removed!')) // Success message
+        saveNotes(notesToKeep) // Save updated notes array
     } else {
-        console.log(chalk.red.inverse('No note found!'))
+        console.log(chalk.red.inverse('No note found!')) // Error message if no note found
     }    
 }
 
+// Function to list all notes
 const listNotes = () => {
-    const notes = loadNotes()
+    const notes = loadNotes() // Load existing notes
 
-    console.log(chalk.inverse('Your notes'))
+    console.log(chalk.inverse('Your notes')) // Header for notes list
 
     notes.forEach((note) => {
-        console.log(note.title)
+        console.log(note.title) // Print each note title
     })
 }
 
+// Function to read a specific note
 const readNote = (title) => {
-    const notes = loadNotes()
-    const note = notes.find((note) => note.title === title)
+    const notes = loadNotes() // Load existing notes
+    const note = notes.find((note) => note.title === title) // Find the note with the given title
 
     if (note) {
-        console.log(chalk.inverse(note.title))
-        console.log(note.body)
+        // If note is found
+        console.log(chalk.inverse(note.title)) // Print note title
+        console.log(note.body) // Print note body
     } else {
-        console.log(chalk.red.inverse('Note not found!'))
+        console.log(chalk.red.inverse('Note not found!')) // Error message if note not found
     }
 }
 
+// Function to save notes to the file system
 const saveNotes = (notes) => {
-    const dataJSON = JSON.stringify(notes)
-    fs.writeFileSync('notes.json', dataJSON)
+    const dataJSON = JSON.stringify(notes) // Convert notes array to JSON string
+    fs.writeFileSync('notes.json', dataJSON) // Write JSON string to 'notes.json' file
 }
 
+// Function to load notes from the file system
 const loadNotes = () => {
     try {
-        const dataBuffer = fs.readFileSync('notes.json')
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
+        const dataBuffer = fs.readFileSync('notes.json') // Read the 'notes.json' file
+        const dataJSON = dataBuffer.toString() // Convert buffer to string
+        return JSON.parse(dataJSON) // Parse JSON string to object
     } catch (e) {
-        return []
+        return [] // Return empty array if file does not exist or error occurs
     }
 }
 
+// Exporting functions to be used in other files
 module.exports = {
     addNote: addNote,
     removeNote: removeNote,
